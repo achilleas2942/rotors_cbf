a4971962ade1e10e9b0146a3692ce792561bb1d7
Achilleas Seisa <achilleas2942@gmail.com>
2025-02-11 09:29:15 +0100

    (1) Add parameters for the number of robots, ROS_MASTER_URI, and ROS_IP (2) Fix git clone urls (3) Add mav_comm repo (4) Fix exports for ROS_MASTER_URI and ROS_IP (5) Add roslaunch command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-set -e
 
 # Source the ROS setup
 source /opt/ros/noetic/setup.bash
+
+# Set the parameter
+Pelican_No=${1:-1}
+ROS_MASTER_IP=${2:-$(hostname -I | awk '{print $1}')}
+ROS_IP=${3:-$ROS_MASTER_IP}
 
 # Pull the latest updates from the repositories
 cd /root/catkin_ws/src
 if [ -d "cf_cbf" ]; then
     cd cf_cbf && git pull && cd ..
 else
-    git clone git@github.com:viswans2132/cf_cbf.git
-    git clone git@github.com:achilleas2942/summer_school_controller.git
+    git clone https://github.com/viswans2132/cf_cbf.git
+    git clone https://github.com/viswans2132/ss_workshop.git
+    git clone https://github.com/ethz-asl/mav_comm.git
 fi
 
 # Build the workspace
@@ -20,11 +25,9 @@ catkin_make
 # Source the workspace
 source /root/catkin_ws/devel/setup.bash
 
-# Export ROS_MASTER_URI
-export ROS_MASTER_URI=http://130.240.96.104:11311
-
-# Automatically detect and export the host IP for ROS_IP
-export ROS_IP=$(ip route | grep default | awk '{print $3}')
+# Export ROS_MASTER_URI and ROS_IP
+export ROS_MASTER_URI=http://"$ROS_MASTER_IP":11311
+export ROS_IP="$ROS_IP"
 
 # Execute the provided command
-exec "$@"
+roslaunch cf_cbf tro_docker_cf_cont.launch namespace:="dcf"$Pelican_No""
